b232580cdbf79b0243feb4ec1286f545ff1256db
AbstractPoo <61809264+AbstractPoo@users.noreply.github.com>
2022-01-26 10:42:13 +0000

    import function

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
 local env = (getgenv and getgenv()) or _G
 
 env.mercuryUI = {
-    importCache = []
+    importCache = [],
+    Library = {}
 }
 
 
-function env:import(path)
+function env.import(path)
     if self.importCache[path] then
         return self.importCache[path]
     end
@@ -13,14 +14,16 @@ function env:import(path)
     local content
 
     if type(path) == "string" then
+        if env.RunService:IsStudio() then
+            return game.ReplicatedStorage[path]
         content = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AbstractPoo/mercury-lib/main/".. path .. "lua"))()
-        return content
     elseif type(path) == "number" then
         content = game:GetObjects("rbxassetid://" .. path)[1]
-        return content
     end
-    
+
     self.importCache[path] = content
 
     return content
 end
+
+return mercuryUI.Library
